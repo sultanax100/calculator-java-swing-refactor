5ed564aa7106318db257624159430822cc7c2d88
Leen Alghamdi <leen.saleh555@gmail.com>
2025-11-05 01:22:09 +0300

    add the facade pattern class and the decorator app.java

--- src/Calc/App.java
+++ src/Calc/App.java
@@ -8,13 +8,16 @@ package Calc;
 public class App {
 
     public static void main(String[] args) {  
-        //deleting this line to test the singleton --> new Calculator().setVisible(true);
-        //testing the sigelton 
         javax.swing.SwingUtilities.invokeLater(() -> {
+
+            //  اختبار الـ Singleton 
             Calculator calculator1 = Calculator.getInstance();
             Calculator calculator2 = Calculator.getInstance();
             System.out.println("same instance? " + (calculator1 == calculator2));
-            calculator1.setVisible(true);
+
+            //  بدل ما نستخدم calculator1.setVisible → نستخدم Facade
+            CalculatorFacade calc = new CalculatorFacade();
+            calc.showUI();  // هذا يعرض الواجهة
         });
     }
 
